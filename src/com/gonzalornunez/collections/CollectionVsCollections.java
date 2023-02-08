265e54b35b1c59882a0011789a7809b2a2438559
Nunez <Gonzalo.Nunez@firstdata.com>
2023-02-07 23:05:09 -0300

    Collection vs Collections

---
+++ src/com/gonzalornunez/collections/CollectionVsCollections.java
@@ -0,0 +1,19 @@
+package com.gonzalornunez.collections;
+
+public class CollectionVsCollections {
+
+    public static void main(String[] args) {
+
+        //Both are from java.util
+
+        //Collection is the root interface from the Collection Framework.
+            //Most classes inherits from this interface.
+            //Like List - Set - Queue
+
+        //Collections is an utility class.
+            //Contains pre-defined methods which can be used from invoking the Collections class.
+            //Such as sort() - reverse() - shuffle() - binarySearch() - disJoint()
+
+
+    }
+}
